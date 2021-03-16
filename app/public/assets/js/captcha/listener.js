22ecdde0402f3cac9167e08e35b44efe28352871
TheBrenny <iam+dev@justbrenny.me>
2021-03-16 12:59:38 +1100

    initial captcha commit

---
+++ app/public/assets/js/captcha/listener.js
@@ -0,0 +1,2 @@
+// listen to events that are occuring on the page and collate them into some sort of minimal javascript.
+// keep only a certain amount of events (ie 100 events total)
---
+++ app/public/captcha/captcha.js
@@ -0,0 +1,6 @@
+function receiveData(req,res,next) {
+    // this function will receive the data, do computations, and will call next if the user is allowed to continue.
+    // Otherwise, the user will be blocked.
+}
+
+module.exports = receiveData;
