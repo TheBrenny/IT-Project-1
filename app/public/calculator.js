5211392c2e68fbd2925c7b40539fbefb27955f47
TheBrenny <iam+dev@justbrenny.me>
2021-04-26 15:29:58 +1000

    refactored code and made it requireable

--- app/public/calculator.js
+++ app/public/calculator.js
@@ -1,115 +1,100 @@
-
-// input = [
-//     {x:1,y:300},
-//     {x:2,y:300},
-//     {x:3,y:300},
-//     {x:4,y:300},
-//     {x:5,y:300},
-//     {x:6,y:300},
-// ]
-input = [
-    { x: 3, y: 1 },
-    { x: 3, y: 4 },
-    { x: 6, y: 7 },
-    { x: 3, y: 2 },
-    { x: 3, y: 6 }
-
-]
-
-//declare the list of x and y
-var list_x = []
-var list_y = []
-// do all the math you need to do
-for (i = 0; i < input.length; i++) {
-    var tmp = input[i];
-    list_x.push(tmp.x);
-    list_y.push(tmp.y);
-}
 //This function is to determine bot by using correlation coeffient
 //calculate Correlation coefficient(the trend of line)
 //r = 1(stright line up ),-1(straight line down),0 (no slop,spread)
 function getRSquared(input) {
-    //calculate avg x
-    var avg_x = 0;
-    for (i = 0; i < list_x.length; i++) {
-        avg_x += list_x[i]
-    }
-    var avg_x = avg_x / list_x.length;//calculate the mean of x
-    //calculate avg y
-    var avg_y = 0;
-    for (i = 0; i < list_y.length; i++) {
-        avg_y += list_y[i]
-    }
-    var avg_y = avg_y / list_y.length; //calculate the mean of y
+    let i = 0; // save memory
 
-    //calculate sx
-    var sx = 0;
-    for (i = 0; i < list_x.length; i++) {
-        sx += Math.pow((list_x[i] - avg_x), 2)
-    }
-    sx = Math.sqrt(sx / (list_x.length - 1))
-    //calculate sy
-    var sy = 0;
-    for (i = 0; i < list_x.length; i++) {
-        sy += Math.pow((list_y[i] - avg_y), 2);
-    }
-    sy = Math.sqrt(sy / (list_y.length - 1))
-
-    //calculate (x-avgx)(y-avgy)
-    var xy = 0
+    // calculate the average x and y coordinates
+    let averageX = 0;
+    let averageY = 0;
     for (i = 0; i < input.length; i++) {
-        xy += ((list_x[i] - avg_x) * (list_y[i] - avg_y))
+        averageX += input[i].x;
+        averageY += input[i].y;
     }
+    averageX /= input.length; //calculate the mean of x
+    averageY /= input.length; //calculate the mean of y
+
+    // calculate sx and sy
+    let sx = 0;
+    let sy = 0;
+    for (i = 0; i < input.length; i++) {
+        sx += Math.pow(input[i].x - averageX, 2);
+        sy += Math.pow(input[i].y - averageY, 2);
+    }
+    sx = Math.sqrt(sx / (input.length - 1));
+    sy = Math.sqrt(sy / (input.length - 1));
+
+    //calculate (x-avgx) * (y-avgy)
+    let xy = 0;
+    for (i = 0; i < input.length; i++) {
+        xy += ((input[i].x - averageX) * (input[i].y - averageY));
+    }
+
     //devide the (xy) by sx * sy
-    var CC = (xy / (sy * sx)) / (input.length - 1); //the correlation of coefficient
+    let CC = (xy / (sx * sy)) / (input.length - 1); //the correlation of coefficient
+    if ((sx * sy) == 0) CC = 1;
+
+    CC = Math.pow(CC, 2);
 
     return CC;
 }
+
 //Efficiency is defined as the ratio of displacment devided by distance.
 //the straight line movement has the ratio closed to 1, while the curve line(like human) has lower efficiency ratio 
 //Distance^2 = (x1 - x2)^2 -(y1-y2)^2
 function getEfficiency(input) {
+    let first = input[0]; //first point(x,y)
+    let last = input[input.length - 1]; //last point (x,y)
 
-    first_point = input[0]               //first point(x,y)
-    last_point = input[(input.length) - 1] //last point (x,y)
-    //calculate displacement
-    displacement = Math.sqrt(Math.pow((first_point.x - last_point.x), 2) + Math.pow((first_point.y - last_point.y), 2))
-    distance = Math.abs(first_point.x - last_point.x) + Math.abs(first_point.y - last_point.y)
-    var efficiency = displacement / distance
+    //calculate displacement, distance and efficiency
+    let displacement = Math.sqrt(Math.pow((first.x - last.x), 2) + Math.pow((first.y - last.y), 2));
+    let distance = Math.abs(first.x - last.x) + Math.abs(first.y - last.y);
+    let efficiency = displacement / distance;
 
-    return efficiency
+    return efficiency;
 }
+
+function getEfficiency(input) {
+    let first = input[0]; //first point(x,y)
+    let last = input[input.length - 1]; //last point (x,y)
+
+    //calculate displacement, distance and efficiency
+    let displacement = getDistance(first, last);
+    let distance = 0;
+    for (let i = 1; i < input.length; i++) distance += getDistance(input[i], input[i - 1]);
+    let efficiency = displacement / distance;
+
+    return efficiency;
+}
+
 //For keystrokes calculation taking time 
 //{x,y} = x is the key, y is time of taking to press keys
 //use (N-1) for the process of computing standard deviation 
 //If the standard deviation is 0. We can determine it is a bot
 function getDeviation(input) {
-    var sum = 0
+    let sum = 0;
 
     //calculate avg y
-    var avg_y = 0;
-    for (i = 0; i < list_y.length; i++) {
-        avg_y += list_y[i]
+    let avgY = 0;
+    for (i = 0; i < input.length; i++) {
+        avgY += input[i].y;
     }
-    var avg_y = avg_y / list_y.length; //calculate the mean of y
+    avgY = avgY / input.length; //calculate the mean of y
 
-    for (i = 0; i < list_y.length; i++) {
-        sum += Math.pow(Math.abs(list_y[i] - avg_y), 2)
+    for (i = 0; i < input.length; i++) {
+        sum += Math.pow(Math.abs(input[i].y - avgY), 2);
     }
-    var SD = Math.sqrt(sum / ((list_y.length) - 1))
+    let SD = Math.sqrt(sum / ((input.length) - 1));
 
-    return SD
-
+    return SD;
 }
 
+function getDistance(a, b) {
+    return Math.sqrt(Math.pow((a.x - b.x), 2) + Math.pow((a.y - b.y), 2));
+}
 
-
-var z = getDeviation(input);
-console.log(z);
-var y = getEfficiency(input);
-console.log(y);
-var x = getRSquared(input);
-console.log(x);
-// module.exports = {
-//     getRSquared
-// }
+module.exports = {
+    getRSquared,
+    getDeviation,
+    getEfficiency,
+};
